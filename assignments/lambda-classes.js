2119ed129942e70971931e83d9bc022ce1bea0d1
Gill Abada <gill@callifi.com>
2019-01-24 14:03:03 -0500

    create person and instructor classes to lambda-classes.js.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,29 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(baseAttrs) {
+        this.name = baseAttrs.name;
+        this.age = baseAttrs.age;
+        this.location = baseAttrs.location;
+        this.gender = baseAttrs.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(insAttrs) {
+        super(insAttrs);
+        this.specialty = insAttrs.specialty;
+        this.favLanguage = insAttrs.favLanguage;
+        this.catchPhrase = insAttrs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
